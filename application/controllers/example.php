1b5a90e9b210f06dc5478e099dc1b5485d114688
Thuan Saraiva <thuansaraiva@gmail.com>
2013-11-06 16:50:53 -0300

    initial commit

---
+++ application/controllers/example.php
@@ -0,0 +1,13 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Example extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('pages/example');
+	}
+
+}
+
+/* End of file example.php */
+/* Location: ./application/controllers/example.php */
---
+++ application/views/pages/example.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>jchart Runner</title>
+
+        <!-- jchart source -->
+        <link rel="stylesheet" type="text/css" href="styles/jChartFX%20Palettes/mocha.css">
+        
+        <script type="text/javascript" src="js/jchartfx.system.js"></script>
+        <script type="text/javascript" src="js/jchartfx.coreBasic.js"></script>
+        <script type="text/javascript" src="js/jchartfx.animation.js"></script>
+    </head>
+    <body>
+	    <div id="grafico" style="width:600px;height:400px"></div>
+	</body>
+	<script type="text/javascript" src="files/js/app.js"></script>
+</html>
