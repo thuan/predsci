b3919dadbe4fab44c16b37ca2da12e5371bfc8f7
Thuan Saraiva <thuansaraiva@gmail.com>
2013-11-07 17:39:42 -0300

    mongodb library

--- application/config/routes.php
+++ application/config/routes.php
@@ -40,7 +40,7 @@
 
 $route['default_controller'] = "welcome";
 $route['404_override'] = '';
-$route['dbinsert'] = "databasecontroller";
+$route['databasecontroller'] = "databasecontroller";
 
 
 /* End of file routes.php */
