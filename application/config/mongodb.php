978970527635bc4b537a1661237d87251f1c3709
Thuan Saraiva <thuansaraiva@gmail.com>
2013-11-08 14:32:11 -0300

    mongodb host db flag set true

--- application/config/mongodb.php
+++ application/config/mongodb.php
@@ -25,4 +25,4 @@ $config['default']['mongo_persist_key']	 = 'ci_persist';
 $config['default']['mongo_replica_set']  = FALSE;
 $config['default']['mongo_query_safety'] = 'safe';
 $config['default']['mongo_suppress_connect_error'] = TRUE;
-$config['default']['mongo_host_db_flag']   = FALSE;
+$config['default']['mongo_host_db_flag']   = TRUE;
--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -19,8 +19,8 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
 		$this->load->library('mongo_db');
+		$this->load->view('welcome_message');
 	}
 }
 
