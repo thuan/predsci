2e369b782a1eb0e80cdee6ffa3cba26e2184bbda
Thuan Saraiva <thuansaraiva@gmail.com>
2013-11-08 17:47:15 -0300

    'set seres and set points chart'

--- js/app/app.js
+++ js/app/app.js
@@ -12,11 +12,16 @@ function carregarGraphico() {
     chart1.getLegendBox().setVisible(true);
     chart1.getAllSeries.setStackedStyle(cfx.Stacked.Normal);
     chart1.setGallery(cfx.Gallery.Bar);
+    
+    data = chart1.getData();
+    data.setSeries(3);
+    data.setPoints(10);
     data = [{ "Month": "AT&T", "Positive": 542, "Neutral": 88, "Negative": 20 },
             { "Month": "IBM", "Positive": 348, "Neutral": 71, "Negative": 11 },
             { "Month": "Kore", "Positive": 35, "Neutral": 0, "Negative": 0 },
             { "Month": "Verizon", "Positive": 621, "Neutral": 88, "Negative": 26}
         ];
+    
     chart1.setDataSource(data);
     divHolder = document.getElementById('grafico');
     chart1.create(divHolder);
