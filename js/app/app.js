401e931ee43df4ea482fd9a097d2f30730300be4
Thuan Saraiva <thuansaraiva@gmail.com>
2013-11-08 16:36:40 -0300

    trying to get json object to wcg verizon service

--- js/app/app.js
+++ js/app/app.js
@@ -3,30 +3,27 @@ var chart1;
 
 function carregarGraphico() {
     "use strict";
-    var series1, series2, data, db, divHolder;   
+    var series1, series2, series3, data, db, divHolder;   
     chart1 = new cfx.Chart();
     chart1.getAnimations().getLoad().setEnabled(true);
     
     chart1.getAxisY().setMin(0);
-    chart1.getAxisY().setMax(20);
+    chart1.getAxisY().setMax(14000);
     
     series1 = chart1.getSeries().getItem(0);
     series2 = chart1.getSeries().getItem(1);
+    series3 = chart1.getSeries().getItem(2);
     series1.setGallery(cfx.Gallery.Bar);
     series2.setGallery(cfx.Gallery.Bar);
+    series3.setGallery(cfx.Gallery.Bar);
     
     data = chart1.getData();
-    data.setSeries(2);
+    data.setSeries(3);
     data.setPoints(10);
+
+    data = $.getJSON(http://wcg-verizon-api-alpha.herokuapp.com/rest/drillable/verizon/wireless/competitorsentiment/crosstab?limit=5&period=week&callback=jQuery17206924738397356123_1383938511329&_=1383938532983
+);
     
-    data = [
-        { "Mês": "Jan", "Alcool (km/l)": 15.9, "Gasolina(km/l)": 10.2 },
-        { "Mês": "Fev", "Alcool (km/l)": 12.3, "Gasolina(km/l)": 11.1 },
-        { "Mês": "Mar", "Alcool (km/l)": 12.6, "Gasolina(km/l)": 10.2 },
-        { "Mês": "Abr", "Alcool (km/l)": 11.9, "Gasolina(km/l)": 10 },
-        { "Mês": "Mai", "Alcool (km/l)": 10.3, "Gasolina(km/l)": 10.6 },
-        { "Mês": "Jun", "Alcool (km/l)": 10.8, "Gasolina(km/l)": 9.8 }
-    ];
     chart1.setDataSource(data);
     divHolder = document.getElementById('grafico');
     chart1.create(divHolder);
