5d9ab72f768c393b6434c86067cbceb6fa63df9d
Thuan Saraiva <thuansaraiva@gmail.com>
2013-11-08 16:57:24 -0300

    json test

--- js/app/app.js
+++ js/app/app.js
@@ -3,27 +3,16 @@ var chart1;
 
 function carregarGraphico() {
     "use strict";
-    var series1, series2, series3, data, db, divHolder;   
+    var data, divHolder;
     chart1 = new cfx.Chart();
     chart1.getAnimations().getLoad().setEnabled(true);
     
     chart1.getAxisY().setMin(0);
     chart1.getAxisY().setMax(14000);
-    
-    series1 = chart1.getSeries().getItem(0);
-    series2 = chart1.getSeries().getItem(1);
-    series3 = chart1.getSeries().getItem(2);
-    series1.setGallery(cfx.Gallery.Bar);
-    series2.setGallery(cfx.Gallery.Bar);
-    series3.setGallery(cfx.Gallery.Bar);
-    
-    data = chart1.getData();
-    data.setSeries(3);
-    data.setPoints(10);
-
+    chart1.setGallery(cfx.Gallery.Bar);
     data = $.getJSON('http://socialpulse.w2oservices.com/livecache/vzw_twitter_topic_share_of_voice_summary_1d.json');
-    
     chart1.setDataSource(data);
+    
     divHolder = document.getElementById('grafico');
     chart1.create(divHolder);
 }
