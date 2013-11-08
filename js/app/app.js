419181a05ae6ab87fae166fe7b4050ae99d48b9f
Thuan Saraiva <thuansaraiva@gmail.com>
2013-11-08 17:43:29 -0300

    'data example'

--- js/app/app.js
+++ js/app/app.js
@@ -9,10 +9,15 @@ function carregarGraphico() {
     
     chart1.getAxisY().setMin(0);
     chart1.getAxisY().setMax(14000);
+    chart1.getLegendBox().setVisible(true);
+    chart1.getAllSeries.setStackedStyle(cfx.Stacked.Normal);
     chart1.setGallery(cfx.Gallery.Bar);
-    data = [{ "Answer": "Yes", "Female": 22000, "Male": 23356 }, { "Answer": "No", "Female": 27000, "Male": 21351 }, { "Answer": "Undecided", "Female": 4823, "Male": 8314}];
+    data = [{ "Month": "AT&T", "Positive": 542, "Neutral": 88, "Negative": 20 },
+            { "Month": "IBM", "Positive": 348, "Neutral": 71, "Negative": 11 },
+            { "Month": "Kore", "Positive": 35, "Neutral": 0, "Negative": 0 },
+            { "Month": "Verizon", "Positive": 621, "Neutral": 88, "Negative": 26}
+        ];
     chart1.setDataSource(data);
-    
     divHolder = document.getElementById('grafico');
     chart1.create(divHolder);
 }
