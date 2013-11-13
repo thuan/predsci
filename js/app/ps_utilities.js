549a03970cd7a4b8dcf0b754feb7d33b0e535491
Thuan Saraiva <thuansaraiva@gmail.com>
2013-11-13 10:45:51 -0300

    jsonp

--- js/app/ps_utilities.js
+++ js/app/ps_utilities.js
@@ -22,6 +22,7 @@
             error: function(jqXHR, textStatus, errorThrown) { 
                 console.log(errorThrown); 
                 console.log(textStatus);
+            }
         });
     };
 
