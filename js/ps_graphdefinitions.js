4e7d7d73162c229476723374d3f52d2244c78dd6
Thuan Saraiva <thuansaraiva@gmail.com>
2013-11-13 20:25:18 -0300

    changes

--- js/ps_graphdefinitions.js
+++ js/ps_graphdefinitions.js
@@ -9,48 +9,29 @@
     ps_graphDefinitions.jsonData = "";
 
     ps_graphDefinitions.buildChart = function (sElementName) {
-
-        var objChart;
-
-        objChart = new cfx.Chart();
-        objChart.getAnimations().getLoad().setEnabled(true);
-
-        objChart.setGallery(cfx.Gallery.Bar);
-        var data = objChart.getData();
-        data.setSeries(3);
-        data.setPoints(10);
-        objChart.getAllSeries().setStackedStyle(cfx.Stacked.Normal);
-        objChart.getLegendBox().setVisible(true);
-
-        //UILayout.CreateTitle(objChart, "Volume & Sentiment");
-
-        var data = ps_graphDefinitions.jsonData;
-
-        objChart.setDataSource(data);
-        var divHolder = document.getElementById(sElementName.div_location);
-        objChart.create(divHolder);
-
-        // UILayout.RemoveWidgetGradient();
-    },
+     
+     };
 
     ps_graphDefinitions.buildPieChart = function (sElementName) {
 
-    },
+    };
 
     ps_graphDefinitions.buildBarChart = function (sElementName) {
 
-    },
+    };
 
-    ps_graphDefinitions.buildLineChart = function (sElementName) {},
+    ps_graphDefinitions.buildLineChart = function (sElementName) {
+    };
 
-    ps_graphDefinitions.buildTwitterActivityMap = function (sElementName) {},
+    ps_graphDefinitions.buildTwitterActivityMap = function (sElementName) {
+    };
 
     ps_graphDefinitions.buildTwitterStream = function (sElementName) {
         var build, totalNumberOfTweet, totalNumberOfTweet_admin, rank, screen_name, status_text, reply_count, status_time_str, date, tweetStreamHtml, statusCount, userName, tweetData, divIndex, adminHtml;
         totalNumberOfTweet = 20;
         totalNumberOfTweet_admin = 15;
         
-        build = {
+         build = {
             timeDifference: function (start) {
                 var startDate, endDate, diff, hours, minutes;
                 startDate = new Date(start);
@@ -80,9 +61,7 @@
                         top = top + 80;
                         $('div[index="' + index + '"]').animate({
                             "top": top + "px"
-                        }, 500, function () {
-                        
-                        });
+                        }, 500);
                     }
                 }
             },
@@ -94,9 +73,7 @@
                         top = top - 80;
                         $('div[index="' + index + '"]').animate({
                             "top": top + "px"
-                        }, 500, function () {
-
-                        });
+                        }, 500);
                     }
                 }
             },
@@ -108,63 +85,12 @@
                         top = top + 80;
                         $('div[index_admin="' + index + '"]').animate({
                             "top": top + "px"
-                        }, 500, function () {
-
-                        });
+                        }, 500);
                     }
                 }
             }
         };
-
-        date = new Date();
-        tweetStreamHtml = "";
-        statusCount = ps_graphDefinitions.jsonData.statuses.length;
-
-        userName = ps_graphDefinitions.jsonData.tag_names;
-        tweetData = ps_graphDefinitions.jsonData.statuses;
-
-        divIndex = 0;
-        adminHtml = "";
-
-        for (i = 0; i < statusCount; i++) {
-            //rank			= tweetData[i].rank;
-            screen_name = tweetData[i].screen_name;
-            status_text = tweetData[i].status_text;
-            img_url = tweetData[i].img_url;
-            tweetTime = tweetData[i].status_time_str;
-            status_time_str = date.getDate(tweetData[i].status_time_str) + "/" + date.getMonth(tweetData[i].status_time_str) + "/" + date.getFullYear(tweetData[i].status_time_str);
-
-            if (divIndex === 0) {
-                sessionStorage.presentTopTweetIndex = 0;
-                sessionStorage.presentTopTweetIndex_admin = 0;
-            }
-
-            tweetStreamHtml += '<div index="' + (divIndex) + '" class="div_tweet" style="top:' + (parseInt(divIndex * 1, 10)).toString() + 'px"><div class="div_tweetImage"><a target="_blank" href="https://twitter.com/' + screen_name + '"><img class="img_dp" src="' + img_url + '"></a></div><div class="div_tweetDescription"><h4><a target="_blank" href="https://twitter.com/' + screen_name + '"> ' + screen_name + '</a></h4><div class="div_tweetTime">' + build.timeDifference(tweetTime) + '</div><div class="div_tweetText">' + build.addlinks(status_text) + '</div></div></div>';
-            adminHtml += '<div index_admin="' + divIndex + '" class="div_tweet" style="top:' + (parseInt(divIndex * 75, 10)).toString() + 'px"><div class="div_tweetImage"><a target="_blank" href="https://twitter.com/' + screen_name + '"><img class="img_dp" src="' + img_url + '"></a></div><div class="div_tweetDescription"><h1><a target="_blank" href="https://twitter.com/' + screen_name + '"> ' + screen_name + '</a></h1><div class="div_tweetTime">' + build.timeDifference(tweetTime) + '</div><div class="div_tweetText">' + build.addlinks(status_text) + '</div></div></div>';
-            divIndex += 1;
-        }
-        $(".div_tweetsMain").html(tweetStreamHtml);
-
-        $(".div_upperArrow").on('click', function () {
-            if ($(this).attr('status') !== "disabled" && $(".div_tweetsMain").html() !== "") build.moveTweetForwordByOne();
-        });
-
-        $(".div_downArrow").on('click', function () {
-            if ($(this).attr('status') !== "disabled" && $(".div_tweetsMain").html() !== "") build.moveTweetBackByOne();
-        });
-
-        var scrollTwitTimer = window.setInterval(function () {
-            if ($(".div_tweetsMain").html() !== "") {
-                build.moveTweetBackByOne();
-                build.moveTweetBackByOne_admin();
-            }
-        }, 10000);
-
-        var getTweetDataTimer = window.setInterval(function () {
-            ps_graphDefinitions.buildTwitterStream();
-            ps_graphDefinitions.buildTwitterStreamMentions();
-        }, 60000);
-
+        
         $("#div_tweeterStream").on('click', function () {
 
             $("#div_tweeterStream").attr('isclicked', '1');
@@ -192,16 +118,14 @@
                     });
 
                 }
-            })
+            });
 
         });
-        $('#myModal').on('hidden', function () {
-            $('#myModal').unbind('show');
-            $("#div_tweeterStream").attr('isclicked', '0');
-        })
-        // Finished Twitter Stream
-    },
+        
+    };
 
-    ps_graphDefinitions.buildKeywordTrending = function (sElementName) {},
+    ps_graphDefinitions.buildKeywordTrending = function (sElementName) {
+    
+    };
 
 }(window.ps_graphDefinitions = window.ps_graphDefinitions || {}, jQuery));
