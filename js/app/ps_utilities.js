dbec18cd550a48f56a7ae878ece3d6035c8ce14c
Thuan Saraiva <thuansaraiva@gmail.com>
2013-11-13 09:43:26 -0300

    modify struture graph

--- js/app/ps_utilities.js
+++ js/app/ps_utilities.js
@@ -1,9 +1,9 @@
 
- /*
-*
-* Utilities resource file
-*
-*/
+/*
+ *
+ * Utilities resource file
+ *
+ */
 
 (function (ps_utilities, $, undefined ){
 
@@ -13,29 +13,15 @@
             type: 'GET',
             cache: true,
             data: "",
-            url: "/" + arrayData.dataURL,
+            url: ajaxCalls.APIgettweets2,
             dataType: 'json',
             success: function(dataResponse) {
-
-                //TODO
-                /*
-                * dataResponse is coming in as extra parameters with comma separated
-                * Need to not use eval. In the psgraphdefinitions need to add a success
-                * event to load the rest of the function
-                *
-                */
-
-                psgraphdefinitions.jsonData = dataResponse;
-
-                eval(arrayData.function + "('" + arrayData.div_location + "')");
-
+                ps_graphDefinitions.jsonData = dataResponse;
+                arrayData.function(arrayData);
             },
-            error: function() { console.log('Error making request'); },
+            error: function(e) { console.log(e); },
             json: 'json'
-         });
+        });
     };
-
-
-
 
 }(window.ps_utilities = window.ps_utilities || {}, jQuery));
