766f45bf785074708bc5427a652a9b16a4fa5f83
Thuan Saraiva <thuansaraiva@gmail.com>
2013-11-14 15:18:42 -0300

    eight 14/11

--- js/ps_graphdefinitions.js
+++ js/ps_graphdefinitions.js
@@ -8,14 +8,14 @@
 
     ps_graphdefinitions.jsonData = "";
 
-
-    ps_graphdefinitions.buildTwitterStream = function (sElementName) {
+    ps_graphdefinitions.buildTwitterStream = function (sElementName) 
+    {
         var mentionsData = {
             dataURL: ajaxCalls.getMentionsJsonData,
             function: ps_twitterUtils.buildMentionsData,
             legend: false
         };
         new ps_utilities.loadJsonpData(mentionsData);
-    };
+    }
 
 }(window.ps_graphdefinitions = window.ps_graphdefinitions || {}, jQuery));
