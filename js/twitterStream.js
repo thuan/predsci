d24e798c0ea0847b783a1b2cd3b543bb710e7573
Thuan Saraiva <thuansaraiva@gmail.com>
2013-11-18 12:02:31 -0300

    first 18/11

--- js/twitterStream.js
+++ js/twitterStream.js
@@ -8,14 +8,14 @@ $(function () {
     
     usersJsonData = {
         dataURL: ajaxCalls.getUsersJsonData,
-        function: ps_twitterUtils.getUsersJsonData,
+        function: ps_graphdefinitions.buildTwitterStream.getUsersJsonData,
         legend: false
     };
     new ps_utilities.loadJsonpData(usersJsonData);
     
     mentionsJsonData = {
         dataURL: ajaxCalls.getMentionsJsonData,
-        function: ps_twitterUtils.getMentionJsonData,
+        function: ps_graphdefinitions.buildTwitterStream.getMentionJsonData,
         legend: false
     };
     new ps_utilities.loadJsonpData(mentionsJsonData);
