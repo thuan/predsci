b3e624f68d4a25375a2b49103c13620850c6232d
Thuan Saraiva <thuansaraiva@gmail.com>
2013-11-18 15:10:44 -0300

    thir 18/11

--- js/ps_graphdefinitions.js
+++ js/ps_graphdefinitions.js
@@ -17,32 +17,29 @@
         divIndex = 0;
         tweetStreamHtml = "";
         adminHtml = "";
+        userName = response.tag_names;
+        tweetData = response.statuses;
+        statusCount = response.statuses.length;
+        for (var i = 0; i < statusCount; i++) {
+            screen_name = tweetData[i].screen_name;
+            status_text = tweetData[i].status_text;
+            img_url = tweetData[i].img_url;
+            tweetTime = tweetData[i].status_time_str;
+            status_time_str = date.getDate(tweetData[i].status_time_str) + "/" + date.getMonth(tweetData[i].status_time_str) + "/" + date.getFullYear(tweetData[i].status_time_str);
 
-        var buildStream = {};
-        buildStream.getMentionsJsonData = function () {
-            userName = response.tag_names;
-            tweetData = response.statuses;
-            statusCount = response.statuses.length;
-            for (var i = 0; i < statusCount; i++) {
-                screen_name = tweetData[i].screen_name;
-                status_text = tweetData[i].status_text;
-                img_url = tweetData[i].img_url;
-                tweetTime = tweetData[i].status_time_str;
-                status_time_str = date.getDate(tweetData[i].status_time_str) + "/" + date.getMonth(tweetData[i].status_time_str) + "/" + date.getFullYear(tweetData[i].status_time_str);
-
-                if (divIndex === 0) {
-                    sessionStorage.presentTopTweetIndex = 0;
-                    sessionStorage.presentTopTweetIndex_admin = 0;
-                }
-                tweetStreamHtml += '<div index="' + (divIndex) + '" class="div_tweet" style="top:' + (parseInt(divIndex * 1, 10)).toString() + 'px"><div class="div_tweetImage"><a target="_blank" href="https://twitter.com/' + screen_name + '"><img class="img_dp" src="' + img_url + '"></a></div><div class="div_tweetDescription"><h4><a target="_blank" href="https://twitter.com/' + screen_name + '"> ' + screen_name + '</a></h4><div class="div_tweetTime">' + ps_twitterUtils.timeDifference(tweetTime) + '</div><div class="div_tweetText">' + ps_twitterUtils.addlinks(status_text) + '</div></div></div>';
-
-                divIndex += 1;
+            if (divIndex === 0) {
+                sessionStorage.presentTopTweetIndex = 0;
+                sessionStorage.presentTopTweetIndex_admin = 0;
             }
-            $("#div_tweeterStream .div_tweetsMain").html(tweetStreamHtml);
-            ps_twitterUtils.buildModals();
-            return buildStream;
-        };
+            tweetStreamHtml += '<div index="' + (divIndex) + '" class="div_tweet" style="top:' + (parseInt(divIndex * 1, 10)).toString() + 'px"><div class="div_tweetImage"><a target="_blank" href="https://twitter.com/' + screen_name + '"><img class="img_dp" src="' + img_url + '"></a></div><div class="div_tweetDescription"><h4><a target="_blank" href="https://twitter.com/' + screen_name + '"> ' + screen_name + '</a></h4><div class="div_tweetTime">' + ps_twitterUtils.timeDifference(tweetTime) + '</div><div class="div_tweetText">' + ps_twitterUtils.addlinks(status_text) + '</div></div></div>';
 
+            divIndex += 1;
+        }
+        $("#div_tweeterStream .div_tweetsMain").html(tweetStreamHtml);
+        ps_twitterUtils.buildModals();
+        
+        var buildStream = {};
+       
         buildStream.getUsersJsonData = function () {
             topTweets = '<table class="table table-bordered"><thead><tr><th>Rank</th><th>Tweet</th><th>Handle</th><th>Reply</th><th>Retweets</th><th>Date</th></tr></thead><tbody>';
             topTweetsModal = '<table class="table table-bordered"><thead><tr><th>Rank</th><th>Tweet</th><th>Handle</th><th>Reply</th><th>Retweets</th><th>Date</th></tr></thead><tbody>';
