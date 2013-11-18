9864a04353969384db280aa266f4ba4f758f50e5
Thuan Saraiva <thuansaraiva@gmail.com>
2013-11-18 16:51:48 -0300

    majority changes

--- inc/header.php
+++ inc/header.php
@@ -2,35 +2,35 @@
 <head>
     <title>Predictive Dashboard</title>
 
-    <script language="javascript" src="lib/jquery/jquery-1.10.2.min.js" type="text/javascript"></script>
-    <script language="javascript" src="js/modals.js" type="text/javascript"></script>
+    <script language="javascript" src="/lib/jquery/jquery-1.10.2.min.js" type="text/javascript"></script>
+    <script language="javascript" src="/js/modals.js" type="text/javascript"></script>
     <script language="javascript" src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=true&libraries=drawing" type="text/javascript" ></script>
 
-    <link href="css/bootstrap/bootstrap.min.css" type="text/css" rel="stylesheet" />
-    <link href="css/bootstrap/jquery.dataTables.css" type="text/css" rel="stylesheet" />
+    <link href="/css/bootstrap/bootstrap.min.css" type="text/css" rel="stylesheet" />
+    <link href="/css/bootstrap/jquery.dataTables.css" type="text/css" rel="stylesheet" />
 
-    <script language="javascript" src="js/ps_utilities.js" type="text/javascript"></script>
-    <script language="javascript" src="js/ps_graphDefinitions.js" type="text/javascript"></script>
-	<script language="javascript" src="js/ps_googlemaps.js" type="text/javascript"></script>
-    <script language="javascript" src="js/wirelessSocialPulse.js" type="text/javascript"></script>
+    <script language="javascript" src="/js/ps_utilities.js" type="text/javascript"></script>
+    <script language="javascript" src="/js/ps_graphDefinitions.js" type="text/javascript"></script>
+	<script language="javascript" src="/js/ps_googlemaps.js" type="text/javascript"></script>
+    <script language="javascript" src="/js/wirelessSocialPulse.js" type="text/javascript"></script>
     
-    <script language="javascript" src="lib/jquery/jquery-ui.js" type="text/javascript"></script>
-    <script language="javascript" src="lib/bootstrap/bootstrap.min.js" type="text/javascript"></script>
-    <script language="javascript" src="lib/bootstrap/jquery.dataTables.min.js" type="text/javascript"></script>
-    <script language="javascript" src="lib/bootstrap/dataTables.bootstrap.js" type="text/javascript"></script>
+    <script language="javascript" src="/lib/jquery/jquery-ui.js" type="text/javascript"></script>
+    <script language="javascript" src="/lib/bootstrap/bootstrap.min.js" type="text/javascript"></script>
+    <script language="javascript" src="/lib/bootstrap/jquery.dataTables.min.js" type="text/javascript"></script>
+    <script language="javascript" src="/lib/bootstrap/dataTables.bootstrap.js" type="text/javascript"></script>
 
 
     <!-- JChart -->
-    <link rel="stylesheet" type="text/css" href="css/jchart/jChartFX/jchartfx.css" />
+    <link rel="stylesheet" type="text/css" href="/css/jchart/jChartFX/jchartfx.css" />
 
-    <script type="text/javascript" src="lib/jchart/jchartfx.system.js"></script>
-    <script type="text/javascript" src="lib/jchart/jchartfx.coreVector.js"></script>
-    <script type="text/javascript" src="lib/jchart/jchartfx.animation.js"></script>
-    <script type="text/javascript" src="lib/jchart/jchartfx.advanced.js"></script>
-    <script type="text/javascript" src="lib/jchart/jchartfx.handdrawn.js"></script>
+    <script type="text/javascript" src="/lib/jchart/jchartfx.system.js"></script>
+    <script type="text/javascript" src="/lib/jchart/jchartfx.coreVector.js"></script>
+    <script type="text/javascript" src="/lib/jchart/jchartfx.animation.js"></script>
+    <script type="text/javascript" src="/lib/jchart/jchartfx.advanced.js"></script>
+    <script type="text/javascript" src="/lib/jchart/jchartfx.handdrawn.js"></script>
 
-    <link rel="stylesheet" type="text/css"  href="css/style.css" />
-    <link href="css/jchart/jChartFX/custom.css" type="text/css" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css"  href="/css/style.css" />
+    <link href="/css/jchart/jChartFX/custom.css" type="text/css" rel="stylesheet" />
     <link href="http://fonts.googleapis.com/css?family=Titillium+Web:400,300,700" rel="stylesheet" type="text/css">
     <link href="http://code.google.com/apis/maps/documentation/javascript/examples/default.css" rel="stylesheet" type="text/css">	
 
