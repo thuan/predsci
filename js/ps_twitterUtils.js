b93e473950b4ac600b02bec738fa85aab388a1dc
Thuan Saraiva <thuansaraiva@gmail.com>
2013-11-21 19:15:19 -0300

    final

--- js/ps_twitterUtils.js
+++ js/ps_twitterUtils.js
@@ -194,13 +194,13 @@
 	            tweetStreamHtml += '<div index="' + (divIndex) + '" class="div_tweet" style="top:' + (parseInt(divIndex * 1, 10)).toString() + 'px"><div class="div_tweetImage"><a target="_blank" href="https://twitter.com/' + screen_name + '"><img class="img_dp" src="' + img_url + '"></a></div><div class="div_tweetDescription"><h4><a target="_blank" href="https://twitter.com/' + screen_name + '"> ' + screen_name + '</a></h4><div class="div_tweetTime">' + $.timeago(tweetTime) + '</div><div class="div_tweetText">' + ps_twitterUtils.addlinks(status_text) + '</div></div></div>';
 	            divIndex += 1;
 	        }
-			$("#div_tweetsMain_admin").html(adminHtml);
-			$("#div_tweetsMain").html(tweetStreamHtml);
+			$("#div_tweeterStream_admin .div_tweetsMain").html(adminHtml);
+			$("#div_tweeterStream .div_tweetsMain").html(tweetStreamHtml);
 		};
 
 	    ps_twitterUtils.buildWidgetScroll = function () {
 	        var scrollTweetTimer = window.setInterval(function () {
-	            if ($("#div_tweetsMain").html() !== "") {
+	            if ($(".div_tweetsMain").html() !== "") {
 	                ps_twitterUtils.moveTweetBackByOne();
 	                ps_twitterUtils.moveTweetBackByOne_admin();
 	            }
